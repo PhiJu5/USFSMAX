88111f1d165eace0df0369bdea0c2a81a7ae16d4
simondlevy <simon.d.levy@gmail.com>
2020-08-04 00:34:41 -0400

    More wiringPi work

--- extras/code/wiringpi/main.cpp
+++ extras/code/wiringpi/main.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <CrossPlatformI2C_Core.h>
-#include <USFSMAX.h>
 
 int main(int argc, char ** argv)
 {
+    uint8_t i2c = cpi2c_open(0x57, 1);
+
+    printf("%d\n", i2c);
+
     return 0;
 }
